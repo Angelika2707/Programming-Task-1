a012148e78c4d6cc69d1b474194efa709702f4ea
Galia <g1l4a>
2023-10-07 01:04:41 +0300

    Step 2: optimality computations

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ x.input_C()
 x.input_A()
 x.input_b()
 x.input_a()
-
 method = SimplexMethod(x.size, x.C, x.A, x.b, x.a)
 method.find_basic_variables()
+method.main()
--- project_classes.py
+++ project_classes.py
@@ -38,11 +38,19 @@ class SimplexMethod:
         self.a = np.array(a)  # The approximation accuracy.
         self.B = np.array([[0 for j in range(self.size[0])] for i in range(self.size[0])])  # basis
         self.B_indexes = [0 for i in range(self.size[0])]  # indexes of basis
+        self.X_b = np.zeros((1, self.size[0]))      # optimal solution vector
+        self.z = 0  # optimal value
+        self.C_b = np.zeros((1, self.size[0]))  # values of non-basic variables in C
+        self.non_basis = [0] * (self.size[1] - self.size[0])    # A vector of non-basic variables
 
     def main(self):
         self.find_basic_variables()
-        B_inverse = self.B.transpose()
+        self.find_non_basic_variables()
+        B_inversed = np.linalg.inv(self.B)
+        entering_vector_inx = self.compute_optimality(B_inversed)
+
         #algorithm
+
         # loop
 
     def find_basic_variables(self):
@@ -53,3 +61,41 @@ class SimplexMethod:
                 if np.array_equal(self.A[:, j], basis.transpose()):
                     self.B[i][i] = 1
                     self.B_indexes[i] = j
+
+    def find_non_basic_variables(self):
+        j = 0
+        for i in range(self.size[0]):
+            if not(i in self.B_indexes):
+                self.non_basis[j] = i
+                j += 1
+
+    # return index of entering vector P_j
+    # with new iteration basis B and indexes is changed
+    # elements in C also changed
+    def compute_optimal_solution(self, B_inversed):
+        b_transposed = self.b.transpose()
+        self.X_b = (B_inversed.dot(b_transposed))
+        self.z = self.C_b.dot(self.X_b.transpose())
+
+    def compute_optimality(self, B_inversed):
+        self.compute_optimal_solution(B_inversed)
+        cur_size = self.size[1] - self.size[0]  # number of non-basic variables
+        z = [0 for i in range(cur_size)]
+        temp_product = self.C_b.dot(B_inversed)
+        flag = False
+        for i in range(cur_size):
+            z[i] = temp_product.dot(self.A[:, self.non_basis[i]]) - self.C.transpose()[self.non_basis[i]]
+            print(z[i])
+            if z[i] >= 0:
+                flag = True
+
+        if flag:
+            return
+            # deliver self.X_b and self.z to output
+        else:
+            min_neg_el = np.min(z)
+            j = z.index(min_neg_el)  # index of entering vector Pj
+            print(self.non_basis[j])
+        return self.non_basis[j]
+
+
